73b04b8f4f0ae29ebacad4f760f220c5c32bee3f
Franklin Wise <franklin@krave-n.com>
2014-12-01 14:06:22 -0800

    update all refences on build

--- definitions/kn_go_build.rb
+++ definitions/kn_go_build.rb
@@ -97,7 +97,7 @@ define :kn_go_build do
 		main_dir = "#{checkout_to}/#{go_main_dir}"
 	end
 
-	execute '/usr/local/go/bin/go get; /usr/local/go/bin/go install;' do 
+	execute '/usr/local/go/bin/go get -u all; /usr/local/go/bin/go install;' do 
 		cwd main_dir
 		environment ({
 			'GOPATH' => "#{go_path}",
