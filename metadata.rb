fb11e46d337ee7dcb4f3803a1cb5f8020e090c44
Franklin Wise <franklin@krave.io>
2015-02-06 15:37:23 -0800

    version

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       'Krave-n, Inc.'
 maintainer_email 'dev-ops@krave-n.com'
 license          'MIT License: Krave-n, Inc'
 description      'utiltiies for installing go apps with chef'
-version          '0.1'
+version          '0.1.1'
 
 
 
