0803908df5b8f43d17a8aa2679eb000043581900
Franklin Wise <franklin@krave-n.com>
2015-02-06 21:15:51 -0800

    path

--- definitions/go_service_directories.rb
+++ definitions/go_service_directories.rb
@@ -1,9 +1,10 @@
 define :go_service_directories do
 	user = params[:user]
 	group = params[:group]
-	deploy_to = params[:deploy_to]
 
-	directory "#{deploy_to}/shared" do
+	deploy_root= "#{params[:deploy_to]}/#{params[:service_name]}"
+
+	directory "#{deploy_root}/shared" do
 		group group
 		owner user
 		mode 0774
@@ -13,7 +14,7 @@ define :go_service_directories do
 
   # create shared/ directory structure
 	['log','config','system','pids','scripts','sockets'].each do |dir_name|
-		directory "#{deploy_to}/shared/#{dir_name}" do
+		directory "#{deploy_root}/shared/#{dir_name}" do
 		group group
 		owner user
 		mode 0774
